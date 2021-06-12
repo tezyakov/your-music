b800dde140ae82cb9ba62389f3042dd9313bb4e3
Nikita Tezyakov <nikitez37@gmail.com>
2021-06-12 19:18:13 +0500

    Added connection to firestore and storage

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import firebase from 'firebase';
 import { StyledFirebaseAuth  } from 'react-firebaseui';
 
+import { app } from '../../base';
 import { Header } from '../../Components/Header/Header';
 import { Layout } from '../../Components/Layout/Layout';
 import { TrackList } from '../TrackList/TrackList';
@@ -9,6 +10,11 @@ import { AddButton } from '../AddButton/AddButton';
 
 export const App: React.FC = () => {
   const [isSignedIn, setIsSignedIn] = React.useState(false);
+  const [fileUrl, setFileUrl] = React.useState(null);
+  const [users, setUsers]: any = React.useState([]);
+
+  const db = app.firestore();
+
   const uiConfig = {
     signInFlow: 'popup',
     signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
@@ -21,12 +27,55 @@ export const App: React.FC = () => {
     firebase.auth().onAuthStateChanged(user => {
       setIsSignedIn(!!user);
     })
+
+    const fetchUsers = async () => {
+      const usersCollection = await db.collection('users').get();
+      setUsers(usersCollection.docs.map((doc) => doc.data()
+      ));
+    }
+
+    fetchUsers();
+    console.log(users[0])
   }, [])
+
+  const onFileChange = async (e: any) => {
+    const file = e.target.files[0];
+    const storageRef = app.storage().ref();
+    const fileRef = storageRef.child(file.name);
+    await fileRef.put(file);
+    setFileUrl(await fileRef.getDownloadURL());
+  }
+
+  const onSumbit = (e: any) => {
+    e.preventDefault();
+    const songName = e.target.songName.value;
+    if (!songName) return;
+    const username = "nikita";
+    console.log(db)
+    db.collection("users").doc(username).set({
+      name: songName,
+      song: fileUrl,
+    })
+  }
+   
   return (
     <div>
       {isSignedIn ? (
         <Layout>
           <button onClick={() => firebase.auth().signOut()}>Sign out</button>
+          <form onSubmit={onSumbit}>
+            <input type="file" onChange={onFileChange} />
+            <input type="text" name="songName" placeholder="Song name" />
+            <button>Sumbit</button>
+          </form>
+          {users.map((user: { name: string; song: string }) => (
+            <div>
+              <p>
+                {user.name}
+                {user.song}
+              </p>
+            </div>
+          ))}
           <Header />
           <AddButton onClick={() => console.log('a')} />
           <TrackList />
---
+++ src/base.ts
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+import 'firebase/storage';
+
+export const app = firebase.initializeApp({
+    apiKey: "AIzaSyBO-9y9DiFUAyqlWghs_pSEHIKkftqWpy8",
+    authDomain: "yourmusic-3ddc6.firebaseapp.com",
+    projectId: "yourmusic-3ddc6",
+    storageBucket: "yourmusic-3ddc6.appspot.com",
+    messagingSenderId: "211242588305",
+    appId: "1:211242588305:web:723e70324014e862d68730",
+    measurementId: "G-EMJJ769J2F"
+  });
